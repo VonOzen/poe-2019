cf861a6f7534910ebbb6b84327c6f166134f4bb2
Simon Escure <hello@simonescure.fr>
2018-12-19 17:02:22 +0100

    added conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ const responsiveNav = require('responsive-nav');
 require('responsive-nav/client/dist/styles/responsive-nav.css');
 
 const nav = responsiveNav('nav');
+
+const aside = document.querySelector('aside');
+if (window.matchMedia("(min-width: 35em)").matches) {
+  fetch('partial.html')
+  .then(function (response) {
+    return response.text()
+  })
+  .then(function (body) {
+    aside.innerHTML = body
+  });
+} else {
+  aside.innerHTML = "<a href='second.html'>Second page</a>"
+}
